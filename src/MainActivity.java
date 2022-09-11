d976d3d2edeb63f889f32fe7192c818e523fb0bb
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-11 22:49:20 +0545

    Init

---
+++ src/MainActivity.java
@@ -0,0 +1,8 @@
+import activities.ActivityFrame;
+
+public class MainActivity {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        new ActivityFrame();
+    }
+}
---
+++ src/activities/ActivityFrame.java
@@ -0,0 +1,12 @@
+package activities;
+
+import javax.swing.*;
+import java.awt.Color;
+
+public class ActivityFrame extends JFrame {
+    public ActivityFrame() {
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.BLACK);
+    }
+}
