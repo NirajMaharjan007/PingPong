fdb15d9267fedd079102e51cf448d58c39119d7e
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-09-12 18:35:37 +0545

    Little Changes

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -2,7 +2,7 @@ import activities.ActivityFrame;
 
 public class MainActivity {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         new ActivityFrame();
+        System.out.println("MainActivity.main()");
     }
 }
